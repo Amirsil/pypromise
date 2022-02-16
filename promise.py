413ee9ce1762a360d23aea6d8877b75dd043cada
unknown <amirsil2002@gmail.com>
2022-02-17 01:20:32 +0200

    Refactor from sleep to observers

--- promise.py
+++ promise.py
@@ -11,67 +11,58 @@ class Promise:
     thread: threading.Thread
     resolver: Callable = None
     rejecter: Callable = None
-    finished: bool = False
-    result = None
-    error = None
+    finish_callback = None
+    
+    def then(self, action: Callable[[Any], None]):
+        self.resolver = action
+        return Promise(self.next_promise_function)
+    
+    def catch(self, exception_handle: Callable[[Exception], None]):
+        self.rejecter = exception_handle
+        return Promise(self.next_promise_function)
     
     def __init__(self, func: Callable[[Callable, Callable], None]):
         def resolve(data):
-            try:
-                if self.resolver:
-                    self.result = self.resolver(data)
-            except Exception as e:
-                self.error = e
+            result, error = None, None
+            
+            if self.resolver:
+                try:
+                    result = self.resolver(data)
+                except Exception as e:
+                    error = e
                 
-            self.finished = True
+            if self.finish_callback and (result or error):
+                self.finish_callback(result, error)
             
         def reject(err):
-            try:
-                if self.rejecter:
-                    self.result = self.rejecter(err)
-                else:
-                    raise err
-            except Exception as e:
-                self.error = e
-                
-            self.finished = True
+            result, error = None, None
+            if self.rejecter:
+                try:
+                    result = self.rejecter(err)
+                except Exception as e:
+                    error = e
+            else:
+                error = err
             
-        def inner():
+            if self.finish_callback and (result or error):
+                self.finish_callback(result, error)
+            
+        def thread_function():
             try:
                 func(resolve, reject)
             except Exception as e:
                 reject(e)
 
-        self.thread = threading.Thread(target=inner)
+        self.thread = threading.Thread(target=thread_function)
         self.thread.start()
-
-    def then(self, action: Callable[[Any], None]):
-        self.resolver = action
-        
-        def inner(resolve, reject):
-            while not self.finished:
-                sleep(0.1)
-            if self.error:
-                reject(self.error)
-            try:
-                resolve(self.result)
-            except Exception as e:
-                reject(e)
-        
-        return Promise(inner)
     
-    def catch(self, exception_handle: Callable[[Exception], None]):
-        self.rejecter = exception_handle
-        
-        def inner(resolve, reject):
-            while not self.finished:
-                sleep(0.1)
-            try:
-                resolve(self.error)
-            except Exception as e:
-                reject(e)
-        
-        return Promise(inner)
+    def next_promise_function(self, resolve, reject):
+        def post_catch_hook(result, error):
+                if error:
+                    reject(error)
+                resolve(result)
+                    
+        self.finish_callback = post_catch_hook
 
 
 def send_url(url):
@@ -94,6 +85,5 @@ Promise(send_url("https://github.com")).then(lambda data: print(data)).catch(lam
 Promise(send_url("https://gsdgsdithub.com")).then(lambda data: print(data)).catch(lambda err: print(f"Error: {err}"))
 Promise(send_url("https://gsdgdsdsgsdgithub.com")).then(lambda data: print(data)).catch(lambda err: print(f"Error: {err}"))
 Promise(send_url("https://github.com")).then(lambda data: print(data)).catch(lambda err: print(f"Error: {err}"))
-Promise(send_url("https://github.com")).then(lambda data: print(data)).catch(lambda err: print(f"Error: {err}"))
-
+Promise(send_url("https://github.com")).then(lambda data: data).then(lambda data: data.json()).catch(lambda err: f"Error: {err}").then(lambda data: print(data))
 sleep(1)
